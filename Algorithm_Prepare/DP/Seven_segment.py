a8cdd2744a209c8bc9434673cb27bcfe50089162
wotj7687 <wotj7687@naver.com>
2021-02-14 00:15:01 +0900

    add solution Seven_segment.py Using DP(Back Tracking)

--- Algorithm_Prepare/DP/Seven_segment.py
+++ Algorithm_Prepare/DP/Seven_segment.py
@@ -1,25 +1,28 @@
 def solution(k):
-    segment = [6,2,5,5,4,5,6,3,7,6]
-    word = []
-    result = [0]
+    word = [0]*(k+1)
+    sword = [0]*8
+    sword[2], sword[3], sword[4], sword[5], sword[6], sword[7] = 1,1,2,5,6,12
 
-    def DFS(k, cost):
-        if word and word[0] == 0: # 맨앞자리 0일때 0 제외 다 되돌아가기
-            return
-        if cost > k:
-            return
-        if cost == k:  # clear
-            result[0] += 1
+    # k 가 6일때는 0만 있는 경우가 생기기 때문에 즉시 리턴
+    if k == 6:
+        return 7
+
+    # 백트래킹을 이용한 DP 풀이 : 맨 마지막 segment (무조건 한 숫자) 에 대해서 왼쪽으로 k-x 만큼 성냥개비가
+    # 있을때까지 계속해서 탐색한다.
+    def DFS(k):
+        if k < 0:
+            return 0
+        if word[k] != 0:
+            return word[k]
+        if k <= 7:
+            return sword[k]
         else:
-            for i in range(10):
-                word.append(i)
-                DFS(k, cost + segment[i])
-                word.pop()
-    DFS(k,0)
-    print(result)
+            word[k] = DFS(k-2) + DFS(k-3) + DFS(k-4) + DFS(k-5)*3 + DFS(k-6)*3 + DFS(k-7)
+            return word[k]
+    return DFS(k)
 
 
 if __name__ == "__main__":
-    r = [5,6,11,1, 25]
+    r = [1,4,11, 25, 50]
     for i in r:
         print(solution(i))
