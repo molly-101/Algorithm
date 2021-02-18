69c415ec110bef292c4b3819c4fddfb97a93aaf4
wotj7687 <wotj7687@naver.com>
2021-02-18 20:22:16 +0900

    modify Maximum_Point.py

--- Algorithm_Prepare/DP/Maximum_Point.py
+++ Algorithm_Prepare/DP/Maximum_Point.py
@@ -6,8 +6,10 @@ sys.stdin = open('input.txt','rt')
 if __name__ == "__main__":
     problems, timelimit = map(int,input().split())
     memo = [0]*(timelimit+1)
+
     for i in range(problems):
         point, time = map(int,input().split())
         for j in range(timelimit,time-1, -1):
             memo[j] = memo[j-time] + point
+
     print(memo[len(memo)-1])
