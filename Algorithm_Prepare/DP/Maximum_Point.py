4b627fc57e212b07aa7b1404e2305064cccdd0d5
wotj7687 <wotj7687@naver.com>
2021-02-18 20:21:22 +0900

    add solution Maximum_Point.py

--- Algorithm_Prepare/DP/Maximum_Point.py
+++ Algorithm_Prepare/DP/Maximum_Point.py
@@ -1,14 +1,13 @@
 import sys
+import copy
 sys.stdin = open('input.txt','rt')
 
 
 if __name__ == "__main__":
-    N, M = map(int,input().split())
-    memo = [0]*(M+1)
-
-    for i in range(N):
+    problems, timelimit = map(int,input().split())
+    memo = [0]*(timelimit+1)
+    for i in range(problems):
         point, time = map(int,input().split())
-        for j in range(M,time-1,-1):
-            if memo[j] < memo[j-time] + point:
-                memo[j] = memo[j-time] + point
-    print(memo)
+        for j in range(timelimit,time-1, -1):
+            memo[j] = memo[j-time] + point
+    print(memo[len(memo)-1])
