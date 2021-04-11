9aba3cd4be97fbe59c2f80bf2208a362071234e5
wotj7687 <wotj7687@icloud.com>
2021-04-12 02:53:24 +0900

    modify BOJ 12865.py solution

--- Algorithm_Prepare/Baekjoon/12865.py
+++ Algorithm_Prepare/Baekjoon/12865.py
@@ -4,6 +4,7 @@ sys.stdin = open('input.txt','rt')
 n, k = map(int, input().split())
 memo = [0]*(k+1)
 
+# W, V 가 고정된 DP
 for i in range(n):
     w, v = map(int,input().split())
     for j in range(k,w-1,-1):
