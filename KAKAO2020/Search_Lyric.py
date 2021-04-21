a01afbd3fd7de1cfe6b52e227384f9f641fdf85c
wotj7687 <wotj7687@icloud.com>
2021-04-21 20:11:02 +0900

    add KAKAO 2020 level 4 가사 검색's solution

--- KAKAO2020/Search_Lyric.py
+++ KAKAO2020/Search_Lyric.py
@@ -1,66 +1,71 @@
-from collections import deque
-
-
-class Node(object):
-    def __init__(self, key, count = 0):
-        self.key = key
-        self.child = {}
-
-
-class Trie(object):
-    def __init__(self):
-        self.head = Node(None)
-
-    def insert(self, word):
-        cur = self.head
-
-        for ch in word:
-            if ch not in cur.child:
-                cur.child[ch] = Node(ch)
-            cur = cur.child[ch]
-        if "*" not in cur.child:
-            cur.child['*'] = 1
-        else:
-            cur.child['*'] += 1
-
-    def search(self, word):
-        cur = self.head
-        Q = deque()
-        Q.append(cur)
-        cnt = 0
-        for ch in word:
-            for i in range(len(Q)):
-                tmp = Q.popleft()
-                if ch == "?":
-                    for i in tmp.child:
-                        if i != "*":
-                            Q.append(tmp.child[i])
-                else:
-                    if ch in tmp.child:
-                        Q.append(tmp.child[ch])
-
-        for i in Q:
-            if '*' in i.child:
-                cnt += i.child['*']
-        return cnt
-
-
 def solution(words, queries):
-    dictionary1 = Trie()
-    dictionary2 = Trie()
-    for i in words:
-        dictionary1.insert(i)
-        dictionary2.insert(i[::-1])
+    Trie = {'num': {}}
+    RTrie = {'num': {}}
     result = []
+
+    # Insert word in Trie and RTrie
+    for word in words:
+        insertTrie(word, Trie, RTrie)
+
+    # find result
     for querie in queries:
-        if querie[0] != "?":
-            dictionary1.search(querie)
+        if querie[0] == "?":
+            result.append(returnResult("".join(reversed(querie)), RTrie))
         else:
-            dictionary2.search(querie[::-1])
+            result.append(returnResult(querie, Trie))
+
     return result
+
+
+def returnResult(querie, T):
+    now = T
+    for i in querie:
+        if i == "?" and len(querie) in now['num']:
+            return now['num'][len(querie)]
+        else:
+            if i not in now:
+                return 0
+            now = now[i]
+
+    return now["num"][len(querie)]
+
+
+def insertTrie(word, Trie, RTrie):
+    now = Trie
+    l_word = len(word)
+    if l_word not in now['num']:
+        now['num'][l_word] = 1
+    else:
+        now['num'][l_word] += 1
+    for i in word:
+        if i not in now:
+            now[i] = {"num": {l_word: 1}}
+        else:
+            if l_word not in now[i]['num']:
+                now[i]["num"][l_word] = 1
+            else:
+                now[i]["num"][l_word] += 1
+
+        now = now[i]
+
+    now = RTrie
+    if l_word not in now['num']:
+        now['num'][l_word] = 1
+    else:
+        now['num'][l_word] += 1
+    for i in reversed(word):
+        if i not in now:
+            now[i] = {"num": {l_word: 1}}
+        else:
+            if l_word not in  now[i]["num"]:
+                now[i]["num"][l_word] = 1
+            else:
+                now[i]["num"][l_word] += 1
+
+        now = now[i]
 
 
 if __name__ == "__main__":
     words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]
-    queries = ["fro??", "????o", "fr???", "fro???", "pro?"]
-    print(solution(words, queries))
+    queries = ["fro??", "????o", "fr???", "fro???", "pro?", "?????", "?????????"]
+    print(solution(words,queries))
