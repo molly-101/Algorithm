462977d94743cab0db661237d52fdc24ee01f4e5
onAuspicious <wotj7687@icloud.com>
2021-06-18 19:57:38 +0900

    modify Programmer's Quad_Zip.py

--- Algorithm_Prepare/Programmers/Quad_Zip.py
+++ Algorithm_Prepare/Programmers/Quad_Zip.py
@@ -7,28 +7,22 @@ def solution(arr):
 
 def quad_zip(res, result):
     if len(res) > 1:
-
         q1 = [i[:len(res) // 2] for i in res[:len(res) // 2]]
         q2 = [i[len(res) // 2:] for i in res[:len(res) // 2]]
         q3 = [i[:len(res) // 2] for i in res[len(res) // 2:]]
         q4 = [i[len(res) // 2:] for i in res[len(res) // 2:]]
         qs = [q1, q2, q3, q4]
 
-        bp = False
+        state = res[0][0]
 
         for j in res:
-            state = j[0]
             for k in j:
                 if k != state:
-                    bp = True
                     for q in qs:
                         quad_zip(q, result)
-                    break
-            if bp:
-                break
+                    return
         else:
             result[state] += 1
-
     else:
         result[res[0][0]] += 1
 
