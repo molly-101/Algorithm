36c226e8a1b683bdb99b27ab87d71a1ca6eb8a5f
onAuspicious <wotj7687@icloud.com>
2021-06-28 02:25:29 +0900

    add solution : Programmers Travel_Road.py

---
+++ Algorithm_Prepare/Programmers/Travel_Road.py
@@ -0,0 +1,35 @@
+import copy
+
+
+def solution(tickets):
+    memo = {}
+    result = []
+    for ticket in tickets:
+        tmp = "".join(ticket)
+        if tmp not in memo:
+            memo[tmp] = 1
+        else:
+            memo[tmp] += 1
+
+    for i in memo:
+        now = ["ICN"]
+        if i[:3] == "ICN":
+            now.append(i[3:])
+            memo[i] -= 1
+            DFS(1, tickets, memo, now, result)
+            memo[i] += 1
+    return sorted(result)[0]
+
+
+def DFS(level, tickets, memo, now, result):
+    if level == len(tickets):
+        result.append(copy.deepcopy(now))
+    else:
+        for ticket in tickets:
+            tmp = "".join(ticket)
+            if now[-1] == ticket[0] and memo[tmp] > 0:
+                memo[tmp] -= 1
+                now.append(tmp[3:])
+                DFS(level + 1, tickets, memo, now, result)
+                memo[tmp] += 1
+                now.pop()
